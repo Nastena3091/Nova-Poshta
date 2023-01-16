87207f1db14b3440a28b93482589e2c1d9068105
Nastena3091 <nastena20043091@gmail.com>
2023-01-16 14:31:37 +0200

    Day 3 last commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -10,7 +10,10 @@
                 warehouse:'',
                 searchedCity:'',
                 searchedWarehouses:[],
-                searchedWarehouse:''
+                searchedWarehouse:'',
+                area:'',
+                areas:[],
+                findArea:[]
             }
             
         },	 
@@ -18,27 +21,21 @@
             axios.post("https://api.novaposhta.ua/v2.0/json/",{
                 "apiKey": "22e5e73d92a0e5d2768eda097e599888",
                 "modelName": "Address",
-                "calledMethod": "getCities",
-                "methodProperties": {
-                }
-            }).then(res => {
-                    // console.log(res.data.data);
-                    this.cities = res.data.data;
-                    // this.cities.forEach((element)=>{
-                    //     console.log(element.Description)
-                    // })
+                "calledMethod": "getAreas",
+                "methodProperties": {}
+                }).then(res => {
+                    console.log(res.data.data);
+                    this.areas = res.data.data;
                 })
         },
         methods: {
             selectWarehouses(){
-                console.log("test")
                 axios.post("https://api.novaposhta.ua/v2.0/json/",{
                     "apiKey": "22e5e73d92a0e5d2768eda097e599888",
                     "modelName": "Address",
                     "calledMethod": "getWarehouses",
                     "methodProperties": {
-                        "CityName": this.city,
-                        "TypeOfWarehouseRef":''
+                        "CityName": this.city
                     }
                 }).then(res => {
                     console.log(res.data.data);
@@ -48,23 +45,44 @@
                     })
                 })
             },
-            searchCity(){
-                if(this.searchedCity){
-                    axios.post("https://api.novaposhta.ua/v2.0/json/",{
-                        "apiKey": "22e5e73d92a0e5d2768eda097e599888",
-                        "modelName": "Address",
-                        "calledMethod": "getWarehouses",
-                        "methodProperties": {
-                            "CityName": this.searchedCity
-                        }
-                    }).then(res => {
-                        console.log(res.data.data);
-                        this.searchedWarehouses = res.data.data;
-                        this.searchedWarehouses.forEach((element)=>{
-                            console.log(element.Description)
-                        })
-                    })
+            // searchCity(){
+            //     if(this.searchedCity){
+            //         axios.post("https://api.novaposhta.ua/v2.0/json/",{
+            //             "apiKey": "22e5e73d92a0e5d2768eda097e599888",
+            //             "modelName": "Address",
+            //             "calledMethod": "getWarehouses",
+            //             "methodProperties": {
+            //                 "CityName": this.searchedCity
+            //             }
+            //         }).then(res => {
+            //             console.log(res.data.data);
+            //             this.searchedWarehouses = res.data.data;
+            //             this.searchedWarehouses.forEach((element)=>{
+            //                 console.log(element.Description)
+            //             })
+            //         })
+            //     }
+            // },
+            selectCity(){
+                // console.log(this.areas)
+                // this.findArea=this.areas.filter(element => {
+                //     element.Description==this.area
+                // });
+                // console.log(this.findArea)
+                axios.post("https://api.novaposhta.ua/v2.0/json/",{
+                "apiKey": "22e5e73d92a0e5d2768eda097e599888",
+                "modelName": "Address",
+                "calledMethod": "getCities",
+                "methodProperties": {
+                    "AreaRef":this.area
                 }
+            }).then(res => {
+                    // console.log(res.data.data);
+                    this.cities = res.data.data;
+                    // this.cities.forEach((element)=>{
+                    //     console.log(element.Description)
+                    // })
+                })
             }
         },
     }
@@ -72,6 +90,13 @@
 
 <template>
     <div class="example">
+        <div class="block">
+            <p>Область:</p>
+            <select id="city" v-model="area" @change="selectCity">
+                <option value="" disabled selected>Область</option>
+                <option v-for="a in areas" :key="a.Ref" :value="a.Ref">{{a.Description}}</option>
+            </select>
+        </div>
         <div class="block">
             <p>Населений пункт:</p>
             <select id="city" v-model="city" @change="selectWarehouses">
@@ -86,8 +111,13 @@
                 <option v-for="w in warehouses" :key="w.Ref">{{w.Description}}</option>
             </select>
         </div>
+        <div class="block">
+            <p>Ви обрали: <br>
+               Населений пункт: {{city}} <br>
+               Відділення: {{ warehouse }}</p>
+        </div>
     </div>
-    <div class="example">
+    <!-- <div class="example">
         <div class="block">
             <p>Населений пункт:</p>
             <input type="text" v-model.lazy="searchedCity" @keydown.enter="searchCity" placeholder="Населений пункт">
@@ -99,7 +129,12 @@
                 <option v-for="s in searchedWarehouses" :key="s.Ref">{{s.Description}}</option>
             </select>
         </div>
-    </div>
+        <div class="block">
+            <p>Ви обрали: <br>
+            Населений пункт: {{searchedCity}} <br>
+            Відділення: {{ searchedWarehouse }}</p>
+        </div>
+    </div> -->
 </template>
 
 <style scoped>
